3dd4e41fa43ed64897ebf85763c87ade07618090
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-05-09 09:13:00 -0300

    Adicionada verificação que impede criação de contas com emails repetidos

--- src/services/mysql.js
+++ src/services/mysql.js
@@ -1,7 +1,22 @@
 import {connection} from '../database/mysql.js'
 
+async function checkIfUserAlreadyExists(userEmail){
+    return new Promise((resolve, reject) =>{
+        connection.query('SELECT * FROM tbUser WHERE userEmail = ?', [userEmail], (err, result) =>{
+            if(err){
+                return reject(err)
+            }
+            return resolve(result.length>0)
+        })
+    })
+}
+
 function createUser(userName, userEmail, userPassword){
-    connection.query('INSERT INTO tbUser(userName, userEmail, userPassword) VALUES(?, ?, ?)', [userName, userEmail, userPassword])
+    checkIfUserAlreadyExists(userEmail).then(res => {
+        if(!res){
+            connection.query('INSERT INTO tbUser(userName, userEmail, userPassword) VALUES(?, ?, ?)', [userName, userEmail, userPassword])
+        }
+    })
 }
 function signIn(userEmail, userPassword, callBack){
     const result = {
@@ -10,7 +25,6 @@ function signIn(userEmail, userPassword, callBack){
     }
     connection.query('SELECT userName, userEmail, userPassword FROM tbUser WHERE userEmail= ? AND userPassword = ?', [userEmail, userPassword], (err, results) => {
         if(results[0]){
-            console.log('usuário existe')
             result.sucess = true
             result.user = results[0]
             return callBack(result)
