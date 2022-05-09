e86f3f31bc6a99a6f5df1d204539afecf05e0862
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-05-09 08:36:09 -0300

    Adicionado fluxo para caso o usuário seja inválido

--- src/services/mysql.js
+++ src/services/mysql.js
@@ -4,14 +4,18 @@ function createUser(userName, userEmail, userPassword){
     connection.query('INSERT INTO tbUser(userName, userEmail, userPassword) VALUES(?, ?, ?)', [userName, userEmail, userPassword])
 }
 function signIn(userEmail, userPassword, callBack){
+    const result = {
+        sucess:false,
+        user:null
+    }
     connection.query('SELECT userName, userEmail, userPassword FROM tbUser WHERE userEmail= ? AND userPassword = ?', [userEmail, userPassword], (err, results) => {
-        // console.log(results[0])
         if(results[0]){
             console.log('usuário existe')
-            callBack(results[0])
-
-            return results[0]
-        }  
+            result.sucess = true
+            result.user = results[0]
+            return callBack(result)
+        }
+        callBack(result)
     })
 
 }
