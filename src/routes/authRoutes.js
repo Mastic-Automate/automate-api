a71febf7eb1dc685537cb0f35b1a3374935c72f6
Gabriel C. Brandão <62272513+GabrielBrandao13@users.noreply.github.com>
2022-05-15 19:37:46 -0300

    Adicionadas verificações para que a rota de signup não aceite entradas inválidas

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,22 +1,46 @@
 import {Router} from 'express'
 
+import {check, validationResult} from 'express-validator'
+
 const authRoutes = Router()
 
 import {signIn, createUser} from '../services/mysql.js'
 
-authRoutes.post('/signup', (req, res) => {
+function handleSignUpRoute(req, res){
+    const errors = validationResult(req)
     const {userName, userEmail, userPassword} = req.body
-    createUser(userName, userEmail, userPassword).then((sucess) =>{
-        return res.json({
-            sucess:sucess,
+    if(errors.isEmpty()){
+        createUser(userName, userEmail, userPassword).then((sucess) =>{
+            return res.status(200).json({
+                sucess:sucess,
+                user: {
+                    userName, userEmail, userPassword
+                }
+            })
         })
-    })
-})
-authRoutes.post('/signin', (req, res) =>{
+    } else {
+        return res.status(422).jsonp({
+            sucess:false,
+            errors: errors.array(),
+            errorsMsgs: errors.array().map(err => err.msg)
+        })
+    }
+}
+function handleSignInRoute(req, res) {
     const {userEmail, userPassword} = req.body
     signIn(userEmail, userPassword, (result) =>{
         return res.json(result)
     })
-})
+}
+
+authRoutes.post('/signup',
+    [
+        check('userEmail', 'Email inválido').not().isEmpty().isEmail().normalizeEmail(),
+        check('userName', 'Nome inválido').trim().not().isEmpty().isLength({min:4, max:40}).withMessage('Nome de usuário deve ter ao menos 4 caracteres e no máximo 40'),
+        check('userPassword', 'Senha inválida').trim().not().isEmpty().isLength({min:8, max:40}).withMessage('Senha de usuário deve ter ao menos 8 caracteres e no máximo 40')
+    ],
+    handleSignUpRoute
+)
+authRoutes.post('/signin', handleSignInRoute)
 
 export {authRoutes}
