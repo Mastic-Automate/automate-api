963d3c16f678ee11c3c7a96d35d1f6cb85848a91
Gabriel C. Brandão <62272513+GabrielBrandao13@users.noreply.github.com>
2022-05-15 19:51:32 -0300

    Adicionadas verificações para que a rota de signin não aceite entradas inválidas

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -27,20 +27,35 @@ function handleSignUpRoute(req, res){
     }
 }
 function handleSignInRoute(req, res) {
-    const {userEmail, userPassword} = req.body
-    signIn(userEmail, userPassword, (result) =>{
-        return res.json(result)
-    })
+    const errors = validationResult(req)
+    if(errors.isEmpty()){
+        const {userEmail, userPassword} = req.body
+        signIn(userEmail, userPassword, (result) =>{
+            return res.status(200).json(result)
+        })
+    } else{
+        return res.status(422).jsonp({
+            sucess:false,
+            errors: errors.array(),
+            errorsMsgs: errors.array().map(err => err.msg)
+        })
+    }
 }
 
 authRoutes.post('/signup',
     [
-        check('userEmail', 'Email inválido').not().isEmpty().isEmail().normalizeEmail(),
-        check('userName', 'Nome inválido').trim().not().isEmpty().isLength({min:4, max:40}).withMessage('Nome de usuário deve ter ao menos 4 caracteres e no máximo 40'),
-        check('userPassword', 'Senha inválida').trim().not().isEmpty().isLength({min:8, max:40}).withMessage('Senha de usuário deve ter ao menos 8 caracteres e no máximo 40')
+        check('userEmail', 'Email inválido').isEmail().normalizeEmail(),
+        check('userName', 'Nome inválido').isLength({min:4, max:40}).withMessage('Nome de usuário deve ter ao menos 4 caracteres e no máximo 40'),
+        check('userPassword', 'Senha inválida').isLength({min:8, max:40}).withMessage('Senha de usuário deve ter ao menos 8 caracteres e no máximo 40')
     ],
     handleSignUpRoute
 )
-authRoutes.post('/signin', handleSignInRoute)
+authRoutes.post('/signin',
+    [
+        check('userEmail', 'Email inválido').isEmail().normalizeEmail(),
+        check('userPassword', 'Senha inválida').isLength({min:8, max:40}).withMessage('Senha de usuário deve ter ao menos 8 caracteres e no máximo 40')
+    ],
+    handleSignInRoute
+)
 
 export {authRoutes}
