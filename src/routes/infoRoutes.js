de8d8b571e215c5eb81b5b82ae5240283ed0c8b7
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-08-18 16:06:16 -0300

    Corrigido erro em que rota de informações de planta não possuia um valor de contagem default

--- src/routes/infoRoutes.js
+++ src/routes/infoRoutes.js
@@ -4,7 +4,7 @@ import { getPlants } from '../services/mysql/getPlants.js'
 const infoRoutes = Router()
 
 infoRoutes.get('/getPlants', async (req, res) => {
-    const count = Number(req.query.count)
+    const count = Number(req.query.count) || 30
     const plants = await getPlants(count)
     return res.json(plants)
 })
