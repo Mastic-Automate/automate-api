2015b682e3c98b0d867c2bc35de698069e9c7eee
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-08-18 17:12:37 -0300

    Atualizadas rotas de informações das plantas

--- src/routes/infoRoutes.js
+++ src/routes/infoRoutes.js
@@ -1,12 +1,24 @@
 import {Router} from 'express'
 import { getPlants } from '../services/mysql/getPlants.js'
+import { getPlantInfo } from '../services/mysql/getPlantInfo.js'
 
 const infoRoutes = Router()
 
 infoRoutes.get('/getPlants', async (req, res) => {
     const count = Number(req.query.count) || 30
     const plants = await getPlants(count)
-    return res.json(plants)
+    return res.status(200).json(plants)
+})
+
+infoRoutes.get('/getPlant', async (req, res) => {
+    const id = Number(req.query.id)
+    if(!id) {
+        return res.status(404).json({
+            message: 'No id provided'
+        })
+    }
+    const plant = await getPlantInfo(id)
+    return res.status(200).json(plant)
 })
 
 export {infoRoutes}
