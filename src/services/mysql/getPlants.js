b89791ba0ac4817d15473e5b51388348f5c1506f
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-08-19 12:48:09 -0300

    Adicionadas mais informações gerais sobre plantas

--- src/services/mysql/getPlants.js
+++ src/services/mysql/getPlants.js
@@ -2,7 +2,7 @@ import { connection } from "../../database/mysql.js"
 
 export async function getPlants(count){
     return new Promise((resolve, reject) => {
-        connection.query('SELECT plantName, plantAbout, idPlant from tbPlants LIMIT ?', 
+        connection.query('SELECT idPlant, plantName, plantAbout, plantWaterQuantity, plantSize, plantTemperature from tbPlants LIMIT ?', 
         [count], 
         (err, result) => {
             if(!err){
