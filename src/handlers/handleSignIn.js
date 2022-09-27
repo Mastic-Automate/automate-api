d68c1fa4fd412c57214239902f9942aafe39db24
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2022-09-27 11:21:45 +0000

    Implementado token de acesso na rota de autenticação

--- src/handlers/handleSignIn.js
+++ src/handlers/handleSignIn.js
@@ -1,14 +1,28 @@
 import {validationResult} from 'express-validator';
 
+import jwt from 'jsonwebtoken';
+
 import {signIn} from '../services/mysql/signIn.js';
 
-function handleSignIn(req, res) {
+async function handleSignIn(req, res) {
     const errors = validationResult(req)
     if(errors.isEmpty()){
         const {userEmail, userPassword} = req.body;
-        signIn(userEmail, userPassword).then(result => {
-            return res.status(200).json(result)
-        });
+        const signResult = await signIn(userEmail, userPassword)
+        const signedUser = signResult.user
+
+        const authToken = jwt.sign(
+            {
+                userId: signedUser.idUser,
+                userEmail: signedUser.userEmail
+            },
+            process.env.TOKEN_SECRET
+        )
+
+        return res
+            .status(200)
+            .cookie('auth-token', authToken)
+            .json(signResult)
     } else{
         return res.status(400).jsonp({
             sucess:false,
